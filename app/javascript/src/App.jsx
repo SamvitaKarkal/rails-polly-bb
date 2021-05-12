ded75aeb2cdbd6439265950f8c445d556f41ea59
SamvitaKarkal <samvitaskarkal@gmail.com>
2021-05-13 01:15:10 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,18 +1,38 @@
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    /*eslint no-undef: "off"*/
     initializeLogger();
-    logger.info("Log from js-logger");
+    registerIntercepts();
+    setAuthHeaders(setLoading);
   }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/polls/:slug/edit" component={EditPoll} />
+        <Route exact path="/polls/create" component={CreatePoll} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        {/* <Route exact path="/sign-up" component={SignUp} /> 
+        <Route exact path="/" component={Login} /> */}
       </Switch>
     </Router>
   );
