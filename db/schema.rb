6455ab30587f2497da55723d38f3b9fcf5ea07ba
SamvitaKarkal <samvitaskarkal@gmail.com>
2021-05-25 15:42:36 +0530

    Added delete option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_083115) do
+ActiveRecord::Schema.define(version: 2021_05_17_153016) do
 
   create_table "options", force: :cascade do |t|
     t.text "content", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_083115) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug", null: false
     t.integer "user_id"
-    t.integer "creator_id"
   end
 
   create_table "responses", force: :cascade do |t|
